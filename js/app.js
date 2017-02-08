d7f0ca2a7a328e40285e9b5b954a95460365c0d9
Lucinde <lucinde@macbook-lucinde.local>
2017-02-08 18:11:12 +0100

    Base
    created player and enemy, moving functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(dx, dy, ds) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = dx;
+    this.y = dy;
+    this.s = ds;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.s;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +28,62 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// Enemies our player must avoid
+var Player = function(col, row) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.row = row;
+    this.col = col;
+  //  this.x = col*colWidth;
+  //  this.y = rowBase+row*rowHeight;
+};
 
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+   this.x = this.col*colWidth;
+   this.y = this.row*rowHeight+rowBase;
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode) {
+    switch (keyCode) {
+      case 'left':
+        this.col -= 1;
+        break;
+      case 'right':
+        this.col += 1;
+        break;
+      case 'up':
+        this.row -= 1;
+        break;
+      case 'down':
+        this.row += 1;
+        break;
+      default:
+    }
+}
+
+// define constants
+var rowHeight = 83;
+var rowBase = -20;
+var colWidth = 101;
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy(0,rowBase+1*rowHeight,10);
+var enemy2 = new Enemy(0,rowBase+2*rowHeight,20);
+var enemy3 = new Enemy(0,rowBase+3*rowHeight,30);
+
+var allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
+var player = new Player(2,5);
 
 
 
